f04ec3fca503c0ba0ffaddf4df80528f540df51b
dan-mutua <danmutua15@gmail.com>
2024-01-04 13:52:27 +0300

    update apps

--- realEstateApi/urls.py
+++ realEstateApi/urls.py
@@ -17,6 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from listing.views import (
+    listing_list,
+    listing_retrieve,
+    listing_create,
+    listing_update,
+    listing_delete
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', listing_list),
+    path('listings/<pk>/', listing_retrieve),
+    path('listings/<pk>/edit/', listing_update),
+    path('listings/<pk>/delete/', listing_delete),
+    path('add-listing/', listing_create)
 ]
